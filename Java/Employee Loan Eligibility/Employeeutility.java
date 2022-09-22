51cdda8c8dfe24a516c9afe31ba9085232218ebc
ishaan019 <69400221+ishaan019@users.noreply.github.com>
2022-09-22 23:28:58 +0530

    Add files via upload

---
+++ Java/Employee Loan Eligibility/Employeeutility.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+ 
+ public class EmployeeUtility {
+ 
+ 	public Employee[] getEmployeeDetails(String[] details) {
+ 		int i=0;
+ 		int n=details.length;
+ 		Employee eobj[]=new Employee[n];
+ 		while(i<n)
+ 		{
+ 			String sarr[]=details[i].split(";");
+ 			String id=sarr[0];
+ 			String name=sarr[1];
+			double sal=Double.parseDouble(sarr[2]);
+ 			Employee eo=new Employee();
+ 			eo.setEmpId(id);
+ 			eo.setEmpName(name);
+ 			eo.setSalary(sal);
+ 			eobj[i]=eo;
+ 			i+=1;
+ 		}
+ 		return(eobj);
+ 	}
+ 
+ 	public Stream<Employee> getStreamOfEmployee(Employee[] empDetails) {
+ 
+ 		
+ 		return(Arrays.stream(empDetails));
+ 	}
+ 
+ 	public String[] shortlistedEmployee(Stream<Employee> empStream,double minSalary) {
+ 
+ 		List<String> sls=new ArrayList<>();
+ 		empStream.filter(e -> e.getSalary()>=minSalary).forEach(x->{
+ 			String res="";
+ 			res+=x.getEmpId()+" "+x.getEmpName()+" "+x.getSalary();
+ 			sls.add(res);
+ 		});;
+ 		String sar[]=new String[sls.size()];
+ 		int j=0;
+ 		for(String ts:sls)
+ 		{
+ 			sar[j]=ts;
+ 			j+=1;
+ 		}
+ 		Arrays.sort(sar);
+ 		return(sar);
+ 	}
+ 
+ }
